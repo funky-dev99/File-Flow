b139db65b110edbe13a66c46501ac8ea7883c35f
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-12-10 16:33:59 +0530

    Do some configs

---
+++ file_flow/src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app"
+// import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMmm5kz6i7UhAYeR07-_B9pG5teq89Ke8",
+    authDomain: "file-flow11.firebaseapp.com",
+    projectId: "file-flow11",
+    storageBucket: "file-flow11.appspot.com",
+    messagingSenderId: "331222125160",
+    appId: "1:331222125160:web:73d9f2aa907307d87bc9dd"
+  };
+
+  const fire = firebase.initializeApp(firebaseConfig); 
